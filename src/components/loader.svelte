9b65011ad01bef48d096e96a247366a9b92aba6a
Tom Westerhuis <tom.westerhuis@flock.community>
2023-12-03 11:53:06 +0100

    Add simple loader

---
+++ src/components/loader.svelte
@@ -0,0 +1,16 @@
+<script>
+    let progress = 0;
+
+    const interval = setInterval(() => {
+        progress += 1;
+        if (progress === 100) {
+            clearInterval(interval);
+        }
+    }, 20);
+</script>
+
+<div class="fixed h-screen w-screen flex items-center justify-center">
+    <div class="w-96 h-4 bg-gray-300">
+        <div class="h-4 bg-red-300" style="width: {progress}%"></div>
+    </div>
+</div>
