16b70985e620d11d958a363098109d651b081319
Tom Westerhuis <tom.westerhuis@flock.community>
2023-12-03 15:02:37 +0100

    Add loader hints

---
+++ src/components/loader-hint.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import hints from "../data/hints.json";
+
+  const FIFTEEN_SECONDS = 15000;
+
+  let hint = hints[Math.floor(Math.random() * hints.length)];
+
+  function refreshHint() {
+    const newHint = hints[Math.floor(Math.random() * hints.length)];
+    if (newHint === hint) {
+      refreshHint();
+      return;
+    }
+    hint = hints[Math.floor(Math.random() * hints.length)];
+  }
+
+  setInterval(() => {
+    refreshHint();
+  }, FIFTEEN_SECONDS);
+</script>
+
+<div class="text-center text-xl">
+  {hint}
+</div>
